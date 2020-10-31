467534d412be8e63cb2d50590e199848ae2825e7
Dhruba Nag <dhrub123@gmail.com>
2020-10-31 05:52:49 +0530

    Master: Lightstep

---
+++ run.sh
@@ -0,0 +1,4 @@
+export LS_ACCESS_TOKEN=IrDCapcMwrsnRJvd8reiM1/QQBiDuIuft6RQ1GwbpA45fUuzg6+jMwQVcugmYWH/vPeeAJddd2zwiY+CBBOIMeWQLEROjROai6v6gDJV
+export LS_SERVICE_NAME=justgifit
+java -javaagent:lightstep-opentelemetry-javaagent-0.9.0.jar \
+     -jar target/*.jar
